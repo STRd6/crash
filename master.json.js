048ec38445c49219259dbd6b00c5137755a5ad01
Daniel X Moore <yahivin@gmail.com>
2014-08-31 17:04:34 -0700

    :id::lock_with_ink_pen: Updated in browser at strd6.github.io/editor

--- master.json.js
+++ master.json.js
@@ -8,24 +8,34 @@ window["STRd6/crash:master"]({
     },
     "main.coffee.md": {
       "path": "main.coffee.md",
-      "content": "We need to bootstrap our whole system.\n\nRun a subshell.\n\nRun a 'GUI' app.\n\nRun a 'Terminal' app.\n\nPipe input to output among running apps.\n\nList running processes.\n\nKill processes.\n\nSTDIO\n\n    alert 'hey'\n",
+      "content": "We need to bootstrap our whole system.\n\n    echo = PACKAGE.distribution.echo.content\n\nRun a command from the cli\n\n    STDOUT = (value) ->\n      # TODO: Do something for real\n      console.log value\n\n    exec = (command) ->\n      [command, args...] = command.split /\\s/\n\n      exe = echo # TODO look up command \n      Function(command, \"ARGV\", \"STDOUT\", exe)(exe, args, STDOUT)\n\nRun a subshell.\n\nRun a 'GUI' app.\n\nRun a 'Terminal' app.\n\n\n    exec 'echo a b'\n\nPipe input to output among running apps.\n\nList running processes.\n\nKill processes.\n\nSTDIO\n",
       "mode": "100644"
     },
     "pixie.cson": {
       "path": "pixie.cson",
       "content": "version: \"0.1.0\"\nremoteDependencies: [\n  \"https://code.jquery.com/jquery-1.11.0.min.js\"\n]\n",
       "mode": "100644"
+    },
+    "echo.coffee.md": {
+      "path": "echo.coffee.md",
+      "content": "Echo\n====\n\nPrint arguments to STDOUT.\n\n    STDOUT ARGV.join(\" \")\n",
+      "mode": "100644"
     }
   },
   "distribution": {
     "main": {
       "path": "main",
-      "content": "(function() {\n  alert('hey');\n\n}).call(this);\n",
+      "content": "(function() {\n  var STDOUT, echo, exec,\n    __slice = [].slice;\n\n  echo = PACKAGE.distribution.echo.content;\n\n  STDOUT = function(value) {\n    return console.log(value);\n  };\n\n  exec = function(command) {\n    var args, exe, _ref;\n    _ref = command.split(/\\s/), command = _ref[0], args = 2 <= _ref.length ? __slice.call(_ref, 1) : [];\n    exe = echo;\n    return Function(command, \"ARGV\", \"STDOUT\", exe)(exe, args, STDOUT);\n  };\n\n  exec('echo a b');\n\n}).call(this);\n",
       "type": "blob"
     },
     "pixie": {
       "path": "pixie",
       "content": "module.exports = {\"version\":\"0.1.0\",\"remoteDependencies\":[\"https://code.jquery.com/jquery-1.11.0.min.js\"]};",
+      "type": "blob"
+    },
+    "echo": {
+      "path": "echo",
+      "content": "(function() {\n  STDOUT(ARGV.join(\" \"));\n\n}).call(this);\n",
       "type": "blob"
     }
   },
