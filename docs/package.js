2cdb967b46915d585b4cd6e6af528f25a1be6607
Daniel X Moore <yahivin@gmail.com>
2014-09-01 17:12:15 -0700

    :curry::triangular_flag_on_post: Updated in browser at strd6.github.io/editor

--- docs/package.js
+++ docs/package.js
@@ -174,7 +174,7 @@
     },
     "main.coffee.md": {
       "path": "main.coffee.md",
-      "content": "We need to bootstrap our whole system.\n\n    echo = PACKAGE.distribution.echo.content\n\nRun a command from the cli\n\n    STDOUT = (value) ->\n      # TODO: Do something for real\n      console.log value\n\n    exec = (command) ->\n      [command, args...] = command.split /\\s/\n\n      exe = echo # TODO look up command \n      Function(\"$PROGRAM_NAME\", \"ARGV\", \"STDOUT\", exe)(command, args, STDOUT)\n\nRun a subshell.\n\nRun a 'GUI' app.\n\nRun a 'Terminal' app.\n\n\n    exec 'echo a b'\n\nPipe input to output among running apps.\n\nList running processes.\n\nKill processes.\n\nSTDIO\n",
+      "content": "We need to bootstrap our whole system.\n\nCommands for testing\n\n    commands = {}\n\n    [\"echo\", \"cat\"].forEach (name) ->\n      commands[name] = PACKAGE.distribution[name].content\n\nRun a command from the cli\n\n    STDOUT = (value) ->\n      # TODO: Do something for real\n      console.log value\n\n    # TODO: Implement for real\n    STDIN = (handler) ->\n\n    exec = (command) ->\n      [command, args...] = command.split /\\s/\n\n      exe = commands[command]\n      Function(\"$PROGRAM_NAME\", \"ARGV\", \"STDOUT\", \"STDIN\", exe)(command, args, STDOUT, STDIN)\n\nRun a subshell.\n\nRun a 'GUI' app.\n\nRun a 'Terminal' app.\n\n    # TODO: Handle pipe\n    exec 'echo hello | cat'\n\nPipe input to output among running apps.\n\nList running processes.\n\nKill processes.\n\nExplore a filesystem.\n\nSTDIO\n",
       "mode": "100644"
     },
     "pixie.cson": {
@@ -186,12 +186,17 @@
       "path": "echo.coffee.md",
       "content": "Echo\n====\n\nPrint arguments to STDOUT.\n\n    STDOUT ARGV.join(\" \")\n",
       "mode": "100644"
+    },
+    "cat.coffee.md": {
+      "path": "cat.coffee.md",
+      "content": "cat\n===\n\nPrint everythnig from STDIN to STDOUT\n\n    STDIN STDOUT\n",
+      "mode": "100644"
     }
   },
   "distribution": {
     "main": {
       "path": "main",
-      "content": "(function() {\n  var STDOUT, echo, exec,\n    __slice = [].slice;\n\n  echo = PACKAGE.distribution.echo.content;\n\n  STDOUT = function(value) {\n    return console.log(value);\n  };\n\n  exec = function(command) {\n    var args, exe, _ref;\n    _ref = command.split(/\\s/), command = _ref[0], args = 2 <= _ref.length ? __slice.call(_ref, 1) : [];\n    exe = echo;\n    return Function(\"$PROGRAM_NAME\", \"ARGV\", \"STDOUT\", exe)(command, args, STDOUT);\n  };\n\n  exec('echo a b');\n\n}).call(this);\n",
+      "content": "(function() {\n  var STDIN, STDOUT, commands, exec,\n    __slice = [].slice;\n\n  commands = {};\n\n  [\"echo\", \"cat\"].forEach(function(name) {\n    return commands[name] = PACKAGE.distribution[name].content;\n  });\n\n  STDOUT = function(value) {\n    return console.log(value);\n  };\n\n  STDIN = function(handler) {};\n\n  exec = function(command) {\n    var args, exe, _ref;\n    _ref = command.split(/\\s/), command = _ref[0], args = 2 <= _ref.length ? __slice.call(_ref, 1) : [];\n    exe = commands[command];\n    return Function(\"$PROGRAM_NAME\", \"ARGV\", \"STDOUT\", \"STDIN\", exe)(command, args, STDOUT, STDIN);\n  };\n\n  exec('echo hello | cat');\n\n}).call(this);\n",
       "type": "blob"
     },
     "pixie": {
@@ -202,6 +207,11 @@
     "echo": {
       "path": "echo",
       "content": "(function() {\n  STDOUT(ARGV.join(\" \"));\n\n}).call(this);\n",
+      "type": "blob"
+    },
+    "cat": {
+      "path": "cat",
+      "content": "(function() {\n  STDIN(STDOUT);\n\n}).call(this);\n",
       "type": "blob"
     }
   },
